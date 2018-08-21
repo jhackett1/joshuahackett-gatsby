605586ba7dc51432339fc9427c4f59953008c1ac
jhackett1 <joshua.hackett93@gmail.com>
2018-08-21 20:16:33 +0100

    Changed smallwins CTA

--- src/components/SmallWinsHero.js
+++ src/components/SmallWinsHero.js
@@ -9,7 +9,7 @@ const SmallWinsHero = () => (
         <div className="container small-wins-hero__inner">
             <h1 className="small-wins-hero__title">Small Wins</h1>
             <h2 className="small-wins-hero__subtitle">Freelance design and development by <strong>Joshua Hackett</strong></h2>
-            <a className="small-wins-hero__link" href="mailto:hello@joshuahackett.com?subject=A new project">Start a project  →</a>
+            <a className="small-wins-hero__link" href="mailto:hello@joshuahackett.com?subject=A new project">Shall we start a project?  →</a>
         </div>
     </section>
 )
